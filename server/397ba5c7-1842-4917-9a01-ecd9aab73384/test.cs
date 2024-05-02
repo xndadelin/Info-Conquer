e3da4f3f9c8d462571dadf70b6f9300950d5a141
xndadelin <adelinmihai071@gmail.com>
2024-05-02 17:28:55 +0300

    docker containerization for C

--- server/397ba5c7-1842-4917-9a01-ecd9aab73384/test.cs
+++
@@ -1,9 +0,0 @@
-namespace Sum00
-{
-    class Hello {         
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("Hello World!");
-        }
-    }
-}            
