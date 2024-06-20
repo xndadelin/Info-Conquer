fe554b8df954cc36ee91813d24897106cf9d5caf
xndadelin <adelinmihai071@gmail.com>
2024-06-20 08:24:28 +0000

    Refactor

--- server/index.js
+++ server/index.js
@@ -13,23 +13,15 @@ async function startServer(){
     
     })
     await apolloServer.start()
-    app.use((req, res, next) => {
-      const allowedOrigin = process.env.MODE === 'dev' ? 'http://localhost:3000' : "https://www.infoconquer.net";
-      res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-      res.setHeader("Access-Control-Allow-Credentials", "true");
-      req.crsfToken = crypto.randomBytes(64).toString('hex');
-      next();
-    });
     app.set('trust proxy', true);
     app.use(cors({
-        origin: ["https://www.infoconquer.net", "http://localhost:3000", "https://studio.apollographql.com"],
+	    origin: ["https://studio.apollographql.com", "http://localhost:3000", "https://infoconquer.net"],
         credentials: 'include'
     }))
+
     apolloServer.applyMiddleware({
         app,
-        cors: false
+        cors: true
     });    
     app.listen(8080, () => {
         console.log('Server started in dev mode! Have fun!')
