f23e4cd023b2ea76cb8a7e04cfe6d7d97e66dd96
xndadelin <adelinmihai071@gmail.com>
2024-06-21 06:06:39 -0500

    Refactor

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,8 +1,10 @@
 import {useQuery, gql} from "@apollo/client";
-import {Card, CardBody, Button} from "@nextui-org/react";
+import {Card, CardBody, Button, User, CardHeader, CardFooter} from "@nextui-org/react";
 import {Link} from "@nextui-org/react";
 import { Loading } from "../components/Loading";
 import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import { UserContext } from "../context/UserContext";
+import { useContext } from "react";
 const newAnnouncements = gql`
     query getAnnouncements {
         getAnnouncements {
@@ -27,20 +29,16 @@ query getHomepageInfo {
 export const Homepage = () => {
     const getAnnouncements = useQuery(newAnnouncements)
     const getInfo = useQuery(getInfos)
+    const {user} = useContext(UserContext)
     if(getAnnouncements.loading) return <Loading/>
     if(getInfos.loading) return <Loading/>
     return (
        <div className="container flex flex-col px-5 mx-auto mt-20 gap-10">
         <div className="flex gap-2 max-md:flex-col justify-between max-md:justify-start">
-            <div className="flex flex-col gap-5 flex-1">
-                    <div className="text-5xl text-white font-extrabold">
-                    🚀 Welcome to{' '}
-                    <span className="from-[#ed3131] to-[#f9f7f5] cursor-pointer bg-clip-text text-transparent bg-gradient-to-b">
-                        InfoConquer!
-                    </span>{' '}
-                    Your ultimate hub for problem-solving!
-                    </div>
-                    <p className="text-2xl text-white">InfoConquer is a platform where you can solve problems, read articles, and compete with other users. You can also create your own contests and invite your friends to join!</p>
+            <div className="font-bold text-3xl">
+                Hi, {user.getUser.username}! 🎉 
+                <br/>
+                Welcome to &lt;InfoConquer/&gt;!
             </div>
             <div className="flex-1 flex flex-col items-end pt-4 w-full">
                <p className="text-2xl font-bold">You can start solving problems by clicking <Link size="lg" isBlock color="danger" href={'/problems'}>here</Link> and choose!</p>
@@ -73,6 +71,61 @@ export const Homepage = () => {
                     </Table>
                 </div>
             </div>
+        </div>
+        <p className="text-3xl font-bold">Deciding what to do?</p>
+        <div className="flex flex-wrap gap-10 justify-center">
+            <Card as={Link} href={"/problems"} className="flex flex-col bg-green-500 hover:bg-green-800 h-full">
+                <CardHeader className="pt-10 flex justify-center items-center">
+                    <svg height={100} xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 640 512">
+                        <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
+                    </svg>
+                </CardHeader>
+                <CardBody>
+                    <p className="text-3xl font-bold">Solve problems?</p>
+                    <p className="text-default-foreground flex justify-center">
+                        Start conquering!
+                    </p>
+                </CardBody>
+            </Card>
+            <Card as={Link} href={"/problems"} className="flex flex-col bg-green-500 hover:bg-green-800 h-full">
+                <CardHeader className="pt-10 flex justify-center items-center">
+                    <svg height={100} xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 640 512">
+                        <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
+                    </svg>
+                </CardHeader>
+                <CardBody>
+                    <p className="text-3xl font-bold">Solve problems?</p>
+                    <p className="text-default-foreground flex justify-center">
+                        Start conquering!
+                    </p>
+                </CardBody>
+            </Card>
+            <Card as={Link} href={"/problems"} className="flex flex-col bg-green-500 hover:bg-green-800 h-full">
+                <CardHeader className="pt-10 flex justify-center items-center">
+                    <svg height={100} xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 640 512">
+                        <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
+                    </svg>
+                </CardHeader>
+                <CardBody>
+                    <p className="text-3xl font-bold">Solve problems?</p>
+                    <p className="text-default-foreground flex justify-center">
+                        Start conquering!
+                    </p>
+                </CardBody>
+            </Card>
+            <Card as={Link} href={"/problems"} className="flex flex-col bg-green-500 hover:bg-green-800 h-full">
+                <CardHeader className="pt-10 flex justify-center items-center">
+                    <svg height={100} xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 640 512">
+                        <path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
+                    </svg>
+                </CardHeader>
+                <CardBody>
+                    <p className="text-3xl font-bold">Solve problems?</p>
+                    <p className="text-default-foreground flex justify-center">
+                        Start conquering!
+                    </p>
+                </CardBody>
+            </Card>
         </div>
         <p className="text-3xl font-bold">Newest annoncements</p>
         <div className="flex flex-col gap-5 max-md:w-[100%] mb-5">
