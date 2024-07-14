b33b1b130c7ba587a21b19f10056ba097d744dc8
xndadelin <adelinmihai071@gmail.com>
2024-07-15 01:09:43 +0300

    Cypress E2E testing for authentication

---
+++ client/cypress/e2e/login.cy.js
@@ -0,0 +1,65 @@
+const password = '';
+
+// to do, environment variables for cypress
+
+describe('Login Page', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/login');
+    });
+
+    it('displays the login form', () => {
+        cy.get('main .text-2xl').should('contain', 'Login');
+        cy.get('input[aria-label="Username or email"]').should('be.visible');
+        cy.get('input[aria-label="Password"]').should('be.visible');
+        cy.get('button[type="submit"]').should('contain', 'Login');
+    });
+
+    it('shows recaptcha', () => {
+        cy.get('.cf-turnstile').should('be.visible');
+    })
+
+    it('shows error message without doing recaptha', () => {
+        cy.get('input[aria-label="Username or email"]').type('invalid_user');
+        cy.get('input[aria-label="Password"]').type('invalid_password');
+        cy.get('button[type="submit"]').click();
+
+        cy.get('span').should('contain', 'Invalid captcha');
+    });
+
+    it('shows error message with invalid credentials and doing recaptha', () => {
+        cy.get('.cf-turnstile').invoke('attr', 'data-sitekey', '1x00000000000000000000AA').should('have.attr', 'data-sitekey', '1x00000000000000000000AA');
+        cy.wait(3000)
+        cy.get('input[aria-label="Username or email"]').type('invalid_user');
+        cy.get('input[aria-label="Password"]').type('invalid_password');
+        cy.get('button[type="submit"]').click();
+    })
+
+    it('displays forgot password link', () => {
+        cy.get('a').contains('Forgot password').should('be.visible').and('have.attr', 'href', '/forgot-password');
+    });
+
+    it('displays register link', () => {
+        cy.get('a').contains('Register').should('be.visible').and('have.attr', 'href', '/register');
+    });
+
+    it('disables login button when inputs are empty', () => {
+        cy.get('button[type="submit"]').should('be.disabled');
+        cy.get('input[aria-label="Username or email"]').type('valid_user');
+        cy.get('input[aria-label="Password"]').type('valid_password');
+        cy.get('button[type="submit"]').should('not.be.disabled');
+    });
+
+    it('should render Discord login button', () => {
+        cy.get('a').contains('Continue with Discord').should('be.visible').and('have.attr', 'href').and('include', 'https://discord.com/oauth2/authorize?client_id=1251926623729488014&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord%2Fcallback&scope=identify')
+    });
+
+    it('redirects to home page on successful login', () => {
+        cy.get('.cf-turnstile').invoke('attr', 'data-sitekey', '1x00000000000000000000AA').should('have.attr', 'data-sitekey', '1x00000000000000000000AA');
+        cy.wait(3000)
+        cy.get('input[aria-label="Username or email"]').type('xnd.adelin');
+        cy.get('input[aria-label="Password"]').type(password)
+        cy.get('button[type="submit"]').click();
+        cy.url().should('eq', 'http://localhost:3000/');
+    });
+
+});
---
+++ client/cypress/e2e/register.cy.js
@@ -0,0 +1,73 @@
+describe('Register Page', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/register');
+    });
+
+    it('displays the registration form', () => {
+        cy.get('main .text-2xl').should('contain', 'Register');
+        cy.get('input[aria-label="Username"]').should('be.visible');
+        cy.get('input[aria-label="Email"]').should('be.visible');
+        cy.get('input[aria-label="Password"]').should('be.visible');
+        cy.get('input[aria-label="Confirm password"]').should('be.visible');
+        cy.get('button[type="submit"]').should('contain', 'Register');
+    });
+
+    it('shows recaptcha', () => {
+        cy.get('.cf-turnstile').should('be.visible');
+    });
+
+    it('invalid email', () => {
+        cy.get('input[aria-label="Username"]').type('new_user');
+        cy.get('input[aria-label="Email"]').type('invalid_email');
+        cy.get('input[aria-label="Password"]').type('password123');
+        cy.get('input[aria-label="Confirm password"]').type('password123');
+        cy.get('button[type="submit"]').click();
+
+        cy.get('span').should('contain', 'Invalid email');
+    });
+
+    it('shows error messages after completing recaptcha', () => {
+
+        cy.get('.cf-turnstile').invoke('attr', 'data-sitekey', '1x00000000000000000000AA');
+        cy.wait(3000);
+        cy.get('input[aria-label="Username"]').type('new_user');
+        cy.get('input[aria-label="Email"]').type('invalid_email');
+        cy.get('input[aria-label="Password"]').type('password123');
+        cy.get('input[aria-label="Confirm password"]').type('password456');
+        cy.get('button[type="submit"]').click();
+
+        cy.get('span').should('contain', 'Passwords do not match. Please submit again with matching passwords.');
+    });
+
+    it('password not strong enough', () => {
+        cy.get('.cf-turnstile').invoke('attr', 'data-sitekey', '1x00000000000000000000AA');
+        cy.wait(3000);
+        cy.get('input[aria-label="Username"]').type('new_user');
+        cy.get('input[aria-label="Email"]').type('valid_email@example.com');
+        cy.get('input[aria-label="Password"]').type('password123');
+        cy.get('input[aria-label="Confirm password"]').type('password123');
+        cy.get('button[type="submit"]').click();
+
+        cy.get('span').should('contain', 'Password must contain at least one number, one lowercase and one uppercase letter, and at least 6 characters long.');
+    });
+
+    it('displays login link', () => {
+        cy.contains('Login').should('be.visible').and('have.attr', 'href', '/login');
+    });
+
+    it('displays discord login button', () => {
+        cy.contains('Continue with Discord')
+            .should('be.visible')
+            .and('have.attr', 'href')
+            .and('include', 'https://discord.com/oauth2/authorize');
+    });
+
+    it('disables register button when inputs are empty', () => {
+        cy.get('button[type="submit"]').should('be.disabled');
+        cy.get('input[aria-label="Username"]').type('valid_user');
+        cy.get('input[aria-label="Email"]').type('valid_email@example.com');
+        cy.get('input[aria-label="Password"]').type('valid_password');
+        cy.get('input[aria-label="Confirm password"]').type('valid_password');
+        cy.get('button[type="submit"]').should('not.be.disabled');
+    });
+});
